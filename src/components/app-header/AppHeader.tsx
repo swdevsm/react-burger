7efac6f45eeecbc98a33a0191fc5f942e44444db
swdevsm <13076163+swdevsm@users.noreply.github.com>
2024-02-19 10:43:53 +0300

    add simple grid and app header component layout

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -1,10 +1,42 @@
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  BurgerIcon,
+  ListIcon,
+  Logo,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import appHeaderStyles from "./AppHeader.module.css";
 
 const AppHeader = () => {
   return (
-    <>
-      <Logo />
-    </>
+    <div className={appHeaderStyles.headerBox}>
+      <div>
+        <div className={appHeaderStyles.headerItem}>
+          <BurgerIcon type="primary" />
+          <p className="text text_type_main-default pl-2">Конструктор</p>
+        </div>
+        <p className="p-1"/>
+        <div className={appHeaderStyles.headerItem}>
+          <ListIcon type="secondary" />
+          <div className="p-1" />
+          <p className="text text_type_main-default text_color_inactive">
+            Лента заказов
+          </p>
+        </div>
+      </div>
+
+      <div className={appHeaderStyles.headerLogo}>
+        <Logo />
+      </div>
+
+      <div className={appHeaderStyles.headerItem}>
+        <ProfileIcon type="secondary" />
+        <div className="p-1" />
+        <p className="text text_type_main-default text_color_inactive">
+          Личный кабинет
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,18 +2,24 @@
 import AppHeader from "../app-header/AppHeader.tsx";
 import BurgerConstructor from "../burger-constructor/BurgerConstructor.tsx";
 import BurgerIngredients from "../burger-ingredients/BurgerIngredients.tsx";
+import appStyles from "./App.module.css";
 
 const App = () => {
-
   return (
-    <>
-      <AppHeader />
-      <hr/>
-      <BurgerConstructor />
-      <hr/>
-      <BurgerIngredients />
-      <hr/>
-    </>
+    <div className={appStyles.container}> 
+      {/* <div className={appStyles.element} id="one"></div> */}
+      <div className={appStyles.nav}>
+        <AppHeader />
+      </div>
+      <div className={appStyles.aside}>
+        <BurgerConstructor />
+      </div>
+      <div className={appStyles.content}>
+        <BurgerIngredients />
+      </div>
+      {/* <div className={appStyles.section}>Section</div> */}
+      {/* <div className={appStyles.footer}>Footer</div> */}
+    </div>
   );
 };
 
