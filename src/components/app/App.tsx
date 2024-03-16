e1911704c21cc9c21581ee3665e5b137474e1d4a
swdevsm <13076163+swdevsm@users.noreply.github.com>
2024-03-16 22:24:54 +0300

    refactor App to Home page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,82 +1,36 @@
-import { useEffect } from "react";
-import AppHeader from "../app-header/AppHeader.tsx";
-import BurgerConstructor from "../burger-constructor/BurgerConstructor.tsx";
-import BurgerIngredients from "../burger-ingredients/BurgerIngredients.tsx";
-import Container from "../container/Container.tsx";
-import Col from "../col/Col.tsx";
-import styles from "../../index.module.css";
-import { useAppSelector, useAppDispatch } from "../../store/hooks.ts";
-import { fetchIngredients } from "../../services/ingredients";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import ForgotPassword from "../../pages/forgot-password/forgot-password";
+import Home from "../../pages/home/home";
+import Ingredient from "../../pages/ingredient/ingredient";
+import Login from "../../pages/login/login";
+import NotFound from "../../pages/not-found/not-found";
+import Profile from "../../pages/profile/profile";
+import Register from "../../pages/register/register";
+import ResetPassword from "../../pages/reset-password/reset-password";
+import { Routes, Route } from "react-router-dom";
+import AppHeader from "../app-header/AppHeader";
 
 const App = () => {
-  const { status } = useAppSelector((state) => state.ingredients);
-  const dispatch = useAppDispatch();
+  return (
+    <>
+      <header>
+        <AppHeader />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ingredient/:id" element={<Ingredient />} />
+        {/* <Route path="/order-history" element={<OrderHistory />} /> */}
+        {/* <Route path="/order-list" element={<OrderList />} /> */}
 
-  useEffect(() => {
-    dispatch(fetchIngredients());
-  }, [dispatch]);
+        <Route path="/not-found" element={<NotFound />} />
 
-  const errorMessage = (
-    <p className="text text_type_main-medium text_color_inactive">
-      Error ... Try to update page
-    </p>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </>
   );
-
-  const loadingMessage = (
-    <p className="text text_type_main-medium text_color_inactive">
-      Loading ... Try to update page
-    </p>
-  );
-
-  try {
-    return (
-      <div>
-        <Container extraClass={styles.center}>
-          <Col w={6}>
-            <header>
-              <AppHeader />
-            </header>
-          </Col>
-          <Col w={6}>
-            <main>
-              {status === "finished" && (
-                <DndProvider backend={HTML5Backend}>
-                  <Container extraClass={styles.center + " pl-10 pr-10"}>
-                    <Col w={3} extraClass={styles.center}>
-                      <BurgerIngredients />
-                    </Col>
-
-                    <Col w={3} extraClass={styles.center + " pl-10"}>
-                      <BurgerConstructor />
-                    </Col>
-                  </Container>
-                </DndProvider>
-              )}
-              {status === "error" && (
-                <Container extraClass={styles.center}>{errorMessage}</Container>
-              )}
-              {status === "loading" && (
-                <Container extraClass={styles.center}>
-                  {loadingMessage}
-                </Container>
-              )}
-            </main>
-          </Col>
-        </Container>
-      </div>
-    );
-  } catch (e) {
-    return (
-      <section>
-        <h1>Что-то пошло не так :(</h1>
-        <p>
-          В приложении произошла ошибка. Пожалуйста, перезагрузите страницу.
-        </p>
-      </section>
-    );
-  }
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/app/App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
-import NewApp from "./components/app/NewApp.tsx";
+import App from "./components/app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        {/* <App /> */}
-        <NewApp />
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
