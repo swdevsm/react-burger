66588b10ba06892c97822ad8a6397fae12f00cd9
swdevsm <13076163+swdevsm@users.noreply.github.com>
2024-04-22 07:52:18 +0300

    correct appheader for select current page as active

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -7,9 +7,12 @@ import {
 import Container from "../container/Container";
 import Col from "../col/Col";
 import styles from "../../index.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const AppHeader = () => {
+  const location = useLocation();
+
+  const inactive = "text_color_inactive";
   return (
     <Container extraClass={styles.center}>
       <Col w={2}>
@@ -17,17 +20,25 @@ const AppHeader = () => {
           <Container extraClass={styles.center}>
             <div className={styles.container + " p-5"}>
               <BurgerIcon type="primary" />
-              <p className="text text_type_main-default pl-2">
+              <p
+                className={`text text_type_main-default pl-2 ${
+                  location.pathname === "/" ? "" : inactive
+                }`}
+              >
                 <Link to={"/"}>Конструктор</Link>
               </p>
             </div>
-            <a href="#" className={styles.container + " p-5 ml-2"}>
+            <div className={styles.container + " p-5 ml-2"}>
               <ListIcon type="secondary" />
               <div className="p-1" />
-              <p className="text text_type_main-default text_color_inactive">
-                Лента заказов
+              <p
+                className={`text text_type_main-default ${
+                  location.pathname === "/profile/orders" ? "" : inactive
+                }`}
+              >
+                <Link to={"/profile/orders"}>Лента заказов</Link>
               </p>
-            </a>
+            </div>
           </Container>
         </nav>
       </Col>
@@ -41,7 +52,11 @@ const AppHeader = () => {
       <Col w={2}>
         <div className={`${styles.container} ${styles.right} p-5`}>
           <ProfileIcon type="secondary" />
-          <p className="text text_type_main-default text_color_inactive pl-1">
+          <p
+            className={`text text_type_main-default pl-1 ${
+              location.pathname === "/profile" ? "" : inactive
+            }`}
+          >
             <Link to={"/profile"}>Личный кабинет</Link>
           </p>
         </div>
